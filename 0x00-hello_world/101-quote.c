bfd3815fdb714d0adbf867f7b0279e6b95d2c667
Manonaahmd <mona2118@yhoo.com>
2023-04-20 14:33:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,31 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - A c program that prints a line to the standard error
-* Return: 1 (success)
- * /
 
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Retern: Always 1.
+ */
+int main(void)
+{
+	write(2,
+              "and that piece of art is useful\" - Dora Korpar,2015-10-19\n",
+	      59);
 	return (1);
 }
