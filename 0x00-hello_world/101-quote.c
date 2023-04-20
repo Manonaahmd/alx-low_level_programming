705d0cbf66900c3497339beabcd9b9de6de7d650
Manonaahmd <mona2118@yhoo.com>
2023-04-20 14:41:55 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,4 @@
 #include <unistd.h>
-
-/**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
- *
- * Return: Always 1.
- */
-int main(void)
-{
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
-	return (1);
-}
-#include <unistd.h>
-
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
