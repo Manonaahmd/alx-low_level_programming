8b78e1240c938277437041ba8ed6d594cd7ccc8b
Manonaahmd <mona2118@yhoo.com>
2023-04-20 10:08:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  *   followed by new line , to the standerd error.
- *   return: Always 0 (success)
+ *   return: Always 1 (success)
  */
 
 int main(void)
