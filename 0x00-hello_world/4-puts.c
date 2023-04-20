251949417fd80e8144d2e23ce9568ccdb826e868
Manonaahmd <mona2118@yhoo.com>
2023-04-20 11:19:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#iniclude <stdio.h>
+
 
 /**
  * main -  entry point
- * Description: prints  "\" programing is like building a multilingual puzzel"
+ * Description: prints  "\" programing is like building a multilingual puzzle"
  * Return : (0)
- * /
+ */
 
 int main(void)
 {
-	puts("\"programming is like building a multilangual puzzel";
+	puts("\"programming is like building a multilangual puzzle";
 	Return :(0)
 {
