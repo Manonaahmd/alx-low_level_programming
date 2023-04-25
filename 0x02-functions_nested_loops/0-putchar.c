a6075cf9096051a128e6eff0d39390a24b18cc97
Manonaahmd <mona2118@yhoo.com>
2023-04-25 06:48:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
+ * main - print _putchar
+ * is written by mbah
  * Return: 0
  */
 
 int main(void)
 {
-	char myVar[] = "_putchar";
-	int i = 0;
-
-	while (myVar[i] != '\0')
-	{
-		_putchar(myVar[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
