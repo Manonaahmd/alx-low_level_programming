821a9a6682c2726f5a97a82b1f63e383569fee36
Manonaahmd <mona2118@yhoo.com>
2023-04-27 05:38:51 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * _isupper-uppercase letters
+ * _isupper - uppercase letters
  * @c: char to check
  *
  * Return: 0 or 1
  */
 
-int_isupper(int c)
+int _isupper(int c)
 {
-	if(c>='A'&&c<='Z')
-		return(1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 	else
-		return(0);
+		return (0);
 }
