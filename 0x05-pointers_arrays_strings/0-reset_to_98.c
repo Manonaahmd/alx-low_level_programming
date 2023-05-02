87fd70f5b29e318b4d501944e46d29abefc4fdc8
Manonaahmd <mona2118@yhoo.com>
2023-05-02 10:38:25 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *);
 
 /**
-  * reset_to_98 - Updates the value it points to 98
-  * @n: pointer to n
-  *
-  * Return: void
-  */
-void reset_to_98(int *n)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	*n = 98;
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
