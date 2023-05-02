a57b99296dde45e222b9c9c0ceb458acb89e27ba
Manonaahmd <mona2118@yhoo.com>
2023-05-02 06:35:32 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,11 +4,11 @@
  * @a: integer to swap
  * @b: integer to swap
  */
-void swap_int(int*a,int *b)
+void swap_int(int *a, int *b)
 {
 	int m;
 
-	m=*a;
-	*a=*b;
-	*b=m;
+	m = *a;
+	*a = *b;
+	*b = m;
 }
