830d87a04234278b84acd6a0bc87ecbd6ae21e60
Manonaahmd <mona2118@yhoo.com>
2023-05-25 02:03:20 -0700

    3-main.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _variadic_h_
+#define _variadic_h_
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
